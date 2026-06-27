4dc39c5b2f3ddbd8bc79f42ebd576bc26ddea43a
vitoriatuba-design <vitria.tuba@gmail.com>
2026-06-27 15:13:49 -0300

    Área de testes

---
+++ src/ManutencaoApiApplication.java
@@ -0,0 +1,68 @@
+package com.exemplo.manutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ManutencaoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManutencaoApiApplication.class, args);
+    }
+}
